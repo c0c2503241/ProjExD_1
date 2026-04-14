0b9c129b0fcac5c421a84c9476b986441b8a7b9a
C0C25032 <c0c2503241@edu.teu.ac.jp>
2026-04-14 16:03:05 +0900

    練習５から９:背景の残像の改善

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,23 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    rbg_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        x = tmr
+        x = x % 3200
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(rbg_img, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
